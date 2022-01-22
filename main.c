a63842e7012e99c2d77deae8ed4a57f646d07214
Amirhossein Aliakbarpour <ah.aliakbarpour@gmail.com>
2022-01-23 01:21:24 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+double determinant(int size, double matrix[size][size])
+{
+    double determinant = 1;
+    int sign = 1;
+
+    // Convert input matrix to triangular matrix by Gaussian elimination method
+    // (The Gaussian elimination method is a little different, so I assume that
+    // there is no solution section)
+    for (int i = 0; i < size - 1; i++)
+        for (int j = i + 1; j < size; j++)
+        {
+            // Check if determinant is equal to zero or not
+            // (If any elements of the main diameter is equal to zero,
+            // and we can't fix it by swapping two rows together the
+            // determinant will be equal to zero)
+            if (matrix[i][i] == 0)
+            {
+                for (int m = 1; i + m < size; m++)
+                {
+                    if (matrix[i + m][i] != 0)
+                    {
+                        // Swap two rows together
+                        double tmp[size - i];
+                        for (int n = i; n < size; n++)
+                        {
+                            tmp[n - i] = matrix[i][n];
+                            matrix[i][n] = matrix[i + m][n];
+                            matrix[i + m][n] = tmp[n - i];
+                        }
+
+                        // (If two rows are swapped, the determinant sign changes)
+                        sign *= -1;
+
+                        break;
+                    }
+                    else if (i + m == size - 1)
+                    {
+                        // Determinant is equal to zero
+                        return 0;
+                    }
+                }
+            }
+
+            // Changing the amount of elements below the main diameter to zero
+            // (To do this the other elements in current row will be affected)
+            if (matrix[j][i] != 0)
+            {
+                double Coefficient = matrix[j][i] / matrix[i][i];
+
+                for (int k = j; k < size; k++)
+                    matrix[j][k] -= Coefficient * matrix[i][k];
+
+                matrix[j][i] = 0;
+            }
+        }
+
+    // Calculate determinant of triangular matrix
+    // (Determinant for triangular matrix is equal to Multiply the
+    // elements on the original diameter)
+    for (int i = 0; i < size; i++)
+        determinant *= matrix[i][i];
+
+    if (determinant == 0)
+        return 0;
+
+    return determinant * sign;
+}
 
 int main()
 {
@@ -7,60 +74,15 @@ int main()
     int size;
     scanf("%d", &size);
 
-    double matrix[size][size], determinant = 1;
-    int signOfDeterminant = 1;
+    double matrix[size][size];
 
     printf("\nEnter the Matrix elements:\n");
 
-    for (int row = 0; row < size; row++)
-        for (int column = 0; column < size; column++)
-            scanf("%lf", &matrix[row][column]);
-
-    // Convert to triangular matrix
     for (int i = 0; i < size; i++)
-        for (int j = i + 1; j < size; j++)
-        {
-            // Check if determinant is equal to zero or not
-            if (matrix[i][i] == 0)
-            {
-                for (int m = 1; i + m < size; m++)
-                    if (matrix[i + m][i] != 0)
-                    {
-                        double hold[size - i];
-                        for (int n = i; n < size; n++)
-                        {
-                            hold[n - i] = matrix[i][n];
-                            matrix[i][n] = matrix[i + m][n];
-                            matrix[i + m][n] = hold[n - i];
-                        }
-                        signOfDeterminant *= -1;
-                        break;
-                    }
-                    else
-                    {
-                        if (i + m == size - 1)
-                        {
-                            // Determinant is equal to zero
-                            determinant = 0, signOfDeterminant = 1;
-                            goto LOOP1;
-                        }
-                        continue;
-                    }
-            }
-            
-            // Changing the amount of some elements to zero
-            double Coefficient = matrix[j][i] / matrix[i][i];
-            for (int k = i + 1; k < size; k++)
-                matrix[j][k] -= Coefficient * matrix[i][k];
-        }
+        for (int j = 0; j < size; j++)
+            scanf("%lf", &matrix[i][j]);
 
-    // Calculate determinant of triangular matrix
-    for (int i = 0; i < size; i++)
-        determinant *= matrix[i][i];
-
-    LOOP1:
-
-    printf("\nThe determinant is: %.2f\n", (float) determinant * signOfDeterminant);
+    printf("\nThe determinant is: %.2f\n", determinant(size, matrix));
 
     return 1;
 }
